0d8986774173e45502c886a4930b6c2194107946
Fredys Matos Borges <websprintt@gmail.com>
2026-06-19 15:45:08 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "/IPower-Gym/",
+    base: "./",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
